a17e715fb7fc9302a47c48175b58edf8d5605473
Concevenancio <concevenancio.32@gmail.com>
2024-03-12 11:48:47 -0600

    borrar y actualizar

--- Backend/helpers/Token.js
+++
@@ -1,6 +0,0 @@
-/*----------TOKEN PARA CONFIRMAR CUENTA------------ */
-const Token = () => {
-    return Date.now().toString(32) + Math.random().toString(32).substring(2);
-};
-
-export default Token;
