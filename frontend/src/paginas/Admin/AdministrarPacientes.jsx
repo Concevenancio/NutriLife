343db344f1b10a850960fcb4da8acb7e48e6533e
AldoZamora <aldooscar123@gmail.com>
2024-04-03 15:52:41 -0600

    w-full formulario

--- frontend/src/paginas/Admin/AdministrarPacientes.jsx
+++ frontend/src/paginas/Admin/AdministrarPacientes.jsx
@@ -3,7 +3,7 @@ import Formulario from "../../components/Formulario";
 const AdministrarPacientes = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="max-w-screen-md w-full">
+      <div className="w-full">
         <Formulario />
       </div>
     </div>
